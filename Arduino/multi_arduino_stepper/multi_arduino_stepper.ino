336b74ebcb3cc7d598ade0e02a658e9ec8a04d41
mliberson <mliberso@usc.edu>
2022-02-18 00:05:30 -0800

    updated arduino code and gui to handle three arduinos

---
+++ Arduino/multi_arduino_stepper/multi_arduino_stepper.ino
@@ -0,0 +1,124 @@
+//Globals
+#define STEPPIN  8
+#define DIRPIN   7
+#define COORDIN0 6
+#define COORDIN1 5
+#define COORDOUT 4
+#define CW 0
+#define CCW 1
+//good frequency for small motor is 10000
+//good frequency for big motor is 5000
+class Driver {
+public:
+
+  int frequency;
+  int STEPTIME;
+  char mode;
+
+  Driver() {}
+  ~Driver() {}
+  void init(char str) 
+  {
+    if(str == 'm') 
+    {
+      this->frequency = 5000;
+    }
+    else
+    {
+      this->frequency = 10000;
+    }
+    setFrequency(this->frequency);
+    mode = str;
+  }
+  void setDirection(int dir) 
+  {
+    if(dir == CW)
+    {
+      digitalWrite(DIRPIN,HIGH);
+    }
+    else
+    {
+      digitalWrite(DIRPIN,LOW);
+    }
+    delayMicroseconds(5);
+  }
+  void setFrequency(int freq)
+  {
+    this->frequency = freq;
+    this->STEPTIME = 1000000/freq;
+  }
+  void handleComm(String command) 
+  {
+    int steps = command.toInt()/0.036;
+    if(steps >= 0) 
+    {
+      setDirection(CW);
+    }
+    else
+    {
+      setDirection(CCW);
+    }
+    coordinate();
+    drive(abs(steps));
+  }
+  void coordinate() 
+  {
+    if(mode == 'm') 
+    {
+      digitalWrite(COORDOUT,HIGH);
+      while(digitalRead(COORDIN0) == 0 || digitalRead(COORDIN1) == 0);
+      digitalWrite(COORDOUT,LOW);
+    }
+    else 
+    {
+      while(digitalRead(COORDIN0) == 0);
+      digitalWrite(COORDOUT,HIGH);
+      while(digitalRead(COORDIN0) == 1);
+      digitalWrite(COORDOUT,LOW);
+    }
+  }
+  void drive(int steps)
+  {
+    for(int i = 0; i < steps; i++) {
+      digitalWrite(STEPPIN,HIGH);
+      delayMicroseconds(3);
+      digitalWrite(STEPPIN,LOW);
+      delayMicroseconds(STEPTIME);
+    }
+  }
+};
+
+Driver myDriver;
+char rec;
+String stuff_right = "";
+String comm_right = "0";
+int timeprev = 0;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(STEPPIN,OUTPUT);
+  pinMode(DIRPIN,OUTPUT);
+  pinMode(COORDIN0,INPUT);
+  pinMode(COORDIN1,INPUT);
+  pinMode(COORDOUT,OUTPUT);
+  digitalWrite(COORDOUT,LOW);
+  Serial.begin(9600);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  while(Serial.available()){
+    rec = Serial.read();
+    if(rec=='m' || rec=='s')
+    {
+      myDriver.init(rec);
+    }
+    else if(rec=='x')
+    {
+      myDriver.handleComm(stuff_right);
+      stuff_right ="";
+    }
+    else
+    {
+      stuff_right+=rec;
+    }
+  }
+}
--- gui/ui.py
+++ gui/ui.py
@@ -8,29 +8,61 @@ class UI(QtWidgets.QMainWindow, Console):
         self.command.clicked.connect(self.send_cmd)
 
     def send_cmd(self):
-        try:
-            joint_1_delta = float(self.joint_1.text())-float(self.joint_1_ang)
-        except:
-            self.terminal.append("Joint 1 command value is invalid.")
 
-        try:
-            joint_2_delta = float(self.joint_2.text())-float(self.joint_2_ang)
-        except:
-            self.terminal.append("Joint 2 command value is invaild.")
-
-        try:
-            joint_3_delta = float(self.joint_2.text())-float(self.joint_2_ang)
-        except:
-            self.terminal.append("Joint 3 command value is invaild.")
-
-        if(self.joint_1.text().isdigit() and self.joint_2.text().isdigit() and self.joint_3.text().isdigit()):
+        if(self.joint_1.text() == 'm' or self.joint_2.text() == 'm' or self.joint_3.text() == 'm'):
             try:
-                cmd = str(self.joint_1.text())+"x"+str(self.joint_2.text())+"y"+str(self.joint_3.text())+"z"
-                self.ser.write(cmd.encode())
+                try:
+                    cmd1 = str(self.joint_1.text())
+                    self.ser1.write(cmd1.encode())
+                except:
+                    self.terminal.append("Connection with the robot x doesn't exist.")
+                try:
+                    cmd2 = str(self.joint_2.text())
+                    self.ser2.write(cmd2.encode())
+                except:
+                    self.terminal.append("Connection with the robot y doesn't exist.")
+                try:
+                    cmd3 = str(self.joint_3.text())
+                    self.ser3.write(cmd3.encode())
+                except:
+                    self.terminal.append("Connection with the robot z doesn't exist.")
             except:
                 self.terminal.append("Connection with the robot doesn't exist.")
+        else:
+            try:
+                joint_1_delta = float(self.joint_1.text())-float(self.joint_1_ang)
+            except:
+                self.terminal.append("Joint 1 command value is invalid.")
 
-    # def calibration(self):
+            try:
+                joint_2_delta = float(self.joint_2.text())-float(self.joint_2_ang)
+            except:
+                self.terminal.append("Joint 2 command value is invaild.")
+
+            try:
+                joint_3_delta = float(self.joint_2.text())-float(self.joint_2_ang)
+            except:
+                self.terminal.append("Joint 3 command value is invaild.")
+
+            if(self.joint_1.text().isdigit() and self.joint_2.text().isdigit() and self.joint_3.text().isdigit()):
+                try:
+                    cmd1 = str(self.joint_1.text())+"x"
+                    self.ser1.write(cmd1.encode())
+                except:
+                    self.terminal.append("Connection with the robot x doesn't exist.")
+                try:
+                    cmd2 = str(self.joint_2.text())+"y"
+                    self.ser2.write(cmd2.encode())
+                except:
+                    self.terminal.append("Connection with the robot y doesn't exist.")
+                try:
+                    cmd3 = str(self.joint_3.text())+"z"
+                    self.ser3.write(cmd3.encode())
+                except:
+                    self.terminal.append("Connection with the robot z doesn't exist.")
+
+    def calibration(self):
+        return True
 
             
 
