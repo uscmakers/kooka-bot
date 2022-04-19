1038e057689275f599daa6d01fc7cc451ce7df33
Spencer Lin <linspenc@usc.edu>
2022-04-18 22:14:38 -0700

    uh I may or may not have borrowed code

--- openCV/ArucoSense.py
+++ openCV/ArucoSense.py
@@ -3,7 +3,7 @@ import cv2.aruco as aruco
   
 def GetCoords():
     
-    cap = cv2.VideoCapture(1)
+    cap = cv2.VideoCapture(0)
     cap.set(4, 480)
     
     # dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
@@ -28,14 +28,14 @@ def GetCoords():
             x_sum = corners[0][0][0][0] + corners[0][0][1][0] + corners[0][0][2][0] + corners[0][0][3][0]
             y_sum = corners[0][0][0][1] + corners[0][0][1][1] + corners[0][0][2][1] + corners[0][0][3][1]
     
-            x_centerPixel = x_sum* .25
-            y_centerPixel = y_sum* .25
+            x_centerPixel = x_sum * .25
+            y_centerPixel = y_sum * .25
 
             coords = (x_centerPixel, y_centerPixel)
 
             print(coords)
             
-            break
+            # break
     
         cv2.imshow("frame", frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
